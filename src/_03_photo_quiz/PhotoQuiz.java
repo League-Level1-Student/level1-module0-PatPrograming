33a09211350c18ff9e81c29541ea8d62b9312f52
League Student <league@iMac-14.attlocal.net>
2020-03-07 11:28:43 -0800

    :up:

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,21 +26,25 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String image=("https://pm1.narvii.com/6864/dae48a3fb515e88ff63430597c11e8d0571d363er1-800-450v2_hq.jpg");
 		// 2. create a variable of type "Component" that will hold your image
-String Component=("https://pm1.narvii.com/6864/dae48a3fb515e88ff63430597c11e8d0571d363er1-800-450v2_hq.jpg");
+Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+component=createImage(image);
 		// 4. add the image to the quiz window
-
+quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String question= JOptionPane.showInputDialog("What is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(question.equals("A God")) {
+	System.out.println("Correct.");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	System.out.println("Incorrect.");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
 
